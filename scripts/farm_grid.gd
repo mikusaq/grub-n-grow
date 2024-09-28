628ac503bf9ab99d34724965e16253d41d3ef8ab
Mikuláš Šťastný <mikusaq@seznam.cz>
2024-09-28 14:11:15 +0300

    Update tile positions, add soil to game

--- scripts/farm_grid.gd
+++ scripts/farm_grid.gd
@@ -2,10 +2,18 @@ extends TileMapLayer
 
 # Positions of tiles in atlas
 const GRASS_POS = Vector2i(0, 0)
-const MULCH_POS = Vector2i(1, 0)
-const SEED_POS = Vector2i(2, 0)
-const PLANT_POS = Vector2i(0, 1)
-const HARVEST_POS = Vector2i(0, 2)
+const SOIL_POS = Vector2i(1, 0)
+const MULCH_POS = Vector2i(2, 0)
+const TREE_POS = Vector2i(3, 0)
+const POTATO_SEED_POS = Vector2i(0, 1)
+const POTATO_PLANT_POS = Vector2i(1, 1)
+const POTATO_HARVEST_POS = Vector2i(2, 1)
+const TOMATO_SEED_POS = Vector2i(0, 2)
+const TOMATO_PLANT_POS = Vector2i(1, 2)
+const TOMATO_HARVEST_POS = Vector2i(2, 2)
+const BASIL_SEED_POS = Vector2i(0, 3)
+const BASIL_PLANT_POS = Vector2i(1, 3)
+const BASIL_HARVEST_POS = Vector2i(2, 3)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -28,6 +36,8 @@ func _input(event: InputEvent) -> void:
 func farm_tile_process_input(tile_pos):
 	var atlas_choords = get_cell_atlas_coords(tile_pos)
 	if atlas_choords == GRASS_POS:
+		set_cell(tile_pos, 0, SOIL_POS)
+	elif atlas_choords == SOIL_POS:
 		set_cell(tile_pos, 0, MULCH_POS)
 	elif atlas_choords == MULCH_POS:
-		set_cell(tile_pos, 0, SEED_POS)
+		pass
