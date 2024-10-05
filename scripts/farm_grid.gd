6cda786ea031f6ec49ab50c281da01cfec4248f1
Mikuláš Šťastný <mikusaq@seznam.cz>
2024-10-05 10:34:09 +0300

    Add tile highlighting based on player position

--- scripts/farm_grid.gd
+++ scripts/farm_grid.gd
@@ -6,9 +6,26 @@ const SOIL_POS = Vector2i(1, 0)
 const MULCH_POS = Vector2i(2, 0)
 const TREE_POS = Vector2i(3, 0)
 
+const CHOORDS_MIN = 0
+const CHOORDS_MAX = 4
+
+const RED_VECTOR: Vector3 = Vector3(0.6, 0.0, 0.0)
+const GREEN_VECTOR: Vector3 = Vector3(0.0, 0.6, 0.0)
+
+@onready var tileMaterial: ShaderMaterial = load("res://shaders/farm_tile_shader_material.material")
+
 signal seed_chosen(plant_type: FarmTile.PlantType)
 	
 var farm_tiles: Array[FarmTile]
+var player_pos: Vector2 = Vector2.ZERO
+var pixel_art_size: Vector2
+var current_tile_pos: Vector2
+
+@export var reach_distance: float = 60.0
+
+
+func set_player_pos(pos: Vector2):
+	player_pos = pos
 
 
 func get_farm_tile(tile_pos: Vector2i) -> FarmTile:
@@ -22,13 +39,22 @@ func get_surrounding_farm_tiles(farm_tile: FarmTile) -> Array[FarmTile]:
 	var farm_tiles: Array[FarmTile]
 	var surrounding_cells = get_surrounding_cells(farm_tile.tile_pos)
 	for cell in surrounding_cells:
-		if cell.x >= 0 and cell.y < 5:
+		if tile_in_farm_grid(cell):
 			farm_tiles.append(get_farm_tile(cell))
 	return farm_tiles
 
 
+func tile_in_farm_grid(farm_tile_pos: Vector2i) -> bool:
+	if (farm_tile_pos.x >= CHOORDS_MIN and farm_tile_pos.x <= CHOORDS_MAX
+	and farm_tile_pos.y >= CHOORDS_MIN and farm_tile_pos.y <= CHOORDS_MAX):
+		return true
+	return false
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	pixel_art_size = self.tile_set.tile_size
+	tileMaterial.set_shader_parameter("pixelArtSize", pixel_art_size.x)
 	var all_cells_pos : Array[Vector2i] = get_used_cells()
 	for tile_pos in all_cells_pos:
 		var farm_tile = FarmTile.new()
@@ -40,12 +66,46 @@ func _ready() -> void:
 		farm_tiles.append(farm_tile)
 
 
+func remove_float_base(num: float, base: int) -> float:
+	return num - fposmod(num, base)
+
+
+func get_tile_global_pos() -> Vector2:
+	var mouse_pos = get_global_mouse_position()
+	var mouseX = remove_float_base(mouse_pos.x, pixel_art_size.x)
+	var mouseY = remove_float_base(mouse_pos.y, pixel_art_size.y)
+	return Vector2(mouseX, mouseY)
+
+
+func tile_pos_changed():
+	if (current_tile_pos.x >= CHOORDS_MIN and current_tile_pos.x <= CHOORDS_MAX * pixel_art_size.x 
+	and current_tile_pos.y >= CHOORDS_MIN and current_tile_pos.y <= CHOORDS_MAX * pixel_art_size.y):
+		var current_tile_pos_center = Vector2(current_tile_pos.x + (pixel_art_size.x/2), current_tile_pos.y + (pixel_art_size.y/2))
+		if current_tile_pos_center.distance_to(player_pos) > reach_distance:
+			tileMaterial.set_shader_parameter("highlightColor", RED_VECTOR)
+		else:
+			tileMaterial.set_shader_parameter("highlightColor", GREEN_VECTOR)
+		print(current_tile_pos)
+		tileMaterial.set_shader_parameter("globalTilePos", current_tile_pos)
+	else:
+		print(Vector2(0,0))
+		tileMaterial.set_shader_parameter("globalTilePos", -pixel_art_size)
+
+
+func _process(delta):
+	if current_tile_pos != get_tile_global_pos():
+		current_tile_pos = get_tile_global_pos()
+		print("change")
+		tile_pos_changed()
+
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_click"):
 		var mouse_pos = get_global_mouse_position()
 		var tile_mouse_pos = local_to_map(mouse_pos)
-		var farm_tile = get_farm_tile(tile_mouse_pos)
-		process_input_on_farm_tile(farm_tile)
+		if tile_in_farm_grid(tile_mouse_pos):
+			var farm_tile = get_farm_tile(tile_mouse_pos)
+			process_click_on_farm_tile(farm_tile)
 	elif event.is_action_pressed("1"):
 		seed_chosen.emit(FarmTile.PlantType.POTATO)
 	elif event.is_action_pressed("2"):
@@ -56,7 +116,7 @@ func _input(event: InputEvent) -> void:
 		process_next_day()
 
 
-func process_input_on_farm_tile(farm_tile: FarmTile):
+func process_click_on_farm_tile(farm_tile: FarmTile):
 	if farm_tile.atlas_choords == GRASS_POS:
 		farm_tile.atlas_choords = SOIL_POS
 	elif farm_tile.atlas_choords == SOIL_POS:
---
+++ scripts/game.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _process(delta: float) -> void:
+	$World/FarmGrid.set_player_pos($Player.position)
