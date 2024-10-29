22f25dcb4493e6a8489337e5abdd24bb6ae4ffc5
Mikuláš Šťastný <mikusaq@seznam.cz>
2024-10-29 12:10:18 +0200

    Add character sprites
    Add character sprites, adjust character collider, add animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
 
-@export var speed = 150
+@export var speed = 100
 
 # Inventory specific
 @export var inv_resource: Inv 
 var active_slot: int
+var playing_animation: bool = false
 
 signal active_slot_changed(new_slot_number: int)
 signal slot_changed(slot_number: int)
@@ -16,7 +17,26 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
+	# Get the input direction vector
 	var input_direction = Input.get_vector("left", "right", "up", "down")
+	
+	# Flip the sprite
+	if input_direction.x > 0:
+		$AnimatedSprite2D.flip_h = false
+	elif input_direction.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	
+	
+	if input_direction.x > 0 or input_direction.x < 0:
+		$AnimatedSprite2D.play("move_right")
+	elif input_direction.y > 0:
+		$AnimatedSprite2D.play("move_down")
+	elif input_direction.y < 0:
+		$AnimatedSprite2D.play("move_up")
+	else:
+		$AnimatedSprite2D.stop()
+	
+	# Apply movement
 	velocity = input_direction * speed
 	move_and_slide()
 
