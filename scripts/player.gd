b1d76fd6d3e2c0a64feb9a5018f6e1ef396ec9b9
Mikuláš Šťastný <mikusaq@seznam.cz>
2024-11-02 12:00:25 +0200

    Add changing active inventory slot with 1-10 keys

--- scripts/player.gd
+++ scripts/player.gd
@@ -41,6 +41,29 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("slot_1"):
+		set_active_slot(0)
+	elif event.is_action_pressed("slot_2"):
+		set_active_slot(1)
+	elif event.is_action_pressed("slot_3"):
+		set_active_slot(2)
+	elif event.is_action_pressed("slot_4"):
+		set_active_slot(3)
+	elif event.is_action_pressed("slot_5"):
+		set_active_slot(4)
+	elif event.is_action_pressed("slot_6"):
+		set_active_slot(5)
+	elif event.is_action_pressed("slot_7"):
+		set_active_slot(6)
+	elif event.is_action_pressed("slot_8"):
+		set_active_slot(7)
+	elif event.is_action_pressed("slot_9"):
+		set_active_slot(8)
+	elif event.is_action_pressed("slot_10"):
+		set_active_slot(9)
+
+
 func set_active_slot(slot_number: int):
 	active_slot = slot_number
 	active_slot_changed.emit(slot_number, get_slot(slot_number).item)
