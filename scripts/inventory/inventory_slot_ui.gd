ce96647ec8a8757e17e9e7bbda81bcdfa6bbdaa5
Mikuláš Šťastný <mikusaq@seznam.cz>
2024-11-02 12:29:55 +0200

    Add amount label to inventory slot ui

--- scripts/inventory/inventory_slot_ui.gd
+++ scripts/inventory/inventory_slot_ui.gd
@@ -4,8 +4,11 @@ func update(slot: InvSlot):
 	if slot and slot.item:
 		$ItemDisplay.texture = slot.item.texture
 		$ItemDisplay.visible = true
+		if slot.item.stackable:
+			$ItemAmount.text = str(slot.amount)
 	else:
 		$ItemDisplay.visible = false
+		$ItemAmount.text = ""
 
 func set_highlight_visibility(highlight_visible: bool):
 	$Highlight.visible = highlight_visible
