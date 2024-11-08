d338a01689310040d970a2dd581e6deae3d2dd34
Mikuláš Šťastný <mikusaq@seznam.cz>
2024-11-08 23:50:03 +0200

    Add task cards resources

---
+++ scripts/tasks/task.gd
@@ -0,0 +1,6 @@
+extends Resource
+
+class_name Task
+
+@export var plant_type: Enums.PlantType
+@export var amount: int
---
+++ scripts/tasks/task_card.gd
@@ -0,0 +1,7 @@
+extends Resource
+
+class_name TaskCard
+
+@export var name: String
+@export var tasks: Array[Task]
+@export var reward: int
