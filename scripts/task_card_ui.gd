8c7503f057bdae499b184f68d171b14177f8ec9b
Mikuláš Šťastný <mikusaq@seznam.cz>
2024-12-05 09:01:39 +0200

    Add fade in/out to task cards

--- scripts/task_card_ui.gd
+++ scripts/task_card_ui.gd
@@ -3,8 +3,15 @@ extends Control
 class_name TaskCardUI
 
 var task_card: TaskCard
+var tween: Tween
 
 signal task_completed(task: TaskCard)
+
+func _ready() -> void:
+	modulate.a = 0.0
+	await get_tree().create_timer(0.1).timeout
+	tween = create_tween()
+	tween.tween_property(self, "modulate:a", 1.0, 0.3)
 
 
 func set_task_card(new_task_card: TaskCard) -> void:
@@ -24,5 +31,8 @@ func update_fulfill_condition() -> void:
 func _on_complete_button_pressed() -> void:
 	$CompleteButton.disabled = true
 	if task_card.fulfill_task():
+		tween = create_tween()
+		tween.tween_property(self, "modulate:a", 0.0, 0.3)
+		await tween.finished
 		task_completed.emit(task_card)
 		queue_free()
